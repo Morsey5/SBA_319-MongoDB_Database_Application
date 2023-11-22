1f2a89179854bf0225e0e465b081c73455804304
Morsey5 <temeikamorse@gmail.com>
2023-11-22 18:49:54 -0500

    Server

---
+++ index.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express()
+const port = 3000
+
+
+// Error Handling Middleware
+app.use((err, req, res, next)=>{
+    res.status(500).send("Something went wrong!")
+})
+app.listen(port, ()=>{
+    console.log(`Server is listening on port: ${port}`)
+})
