db70663f505b513762f66601f703772f0d1ee1f9
Morsey5 <temeikamorse@gmail.com>
2023-11-23 00:15:04 -0500

    GET ONE/ALL

--- routes/inspections.mjs
+++ routes/inspections.mjs
@@ -5,16 +5,37 @@ import { ObjectId } from 'mongodb';
 const router = express.Router();
 
 
-router.get('/', async (req, res) => {
-  try {
-    const db = await connectDB();
-    const inspections = await db.collection('inspections').find({}).toArray();
-    res.json(inspections);
-  } catch (error) {
-    console.error('Error fetching inspections:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// GET ALL
+router.get("/", async (req, res) => {
+    try {
+      const db = await connectDB();
+      const inspections = await db.collection("inspections").find({}).toArray();
+      res.json(inspections);
+    } catch (error) {
+      console.error("Error fetching inspections:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
+  // GET ONE
+  router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const db = await connectDB();
+      const inspection = await db.collection("inspections").findOne({ _id: ObjectId(id) });
+  
+      if (!inspection) {
+        return res.status(404).json({ error: "Inspection not found" });
+      }
+  
+      res.json(inspection);
+    } catch (error) {
+      console.error("Error fetching inspection:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
 
 
 export default router;
